fe35bb52b0f315dc8aa148ec287583b5953270df
O-orion <lucas.lvtorres@gmail.com>
2024-03-04 16:58:55 -0300

    logica inicial carrousel

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,6 +6,45 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+
+  filmexExibidos: any[] = []
+
+  filmes: any[] = [
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/aANgCyN1bRtOp3gvyYuU5kQFp3q.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/nzSJcUFe9a6lF6nxAyqP2cGgYjt.jpg'},
+    {nome: 'Solo', img: 'https://image.tmdb.org/t/p/original/nzSJcUFe9a6lF6nxAyqP2cGgYjt.jpg'},
+  ];
+
+  totalFilmes:number = 8;
+  paginaAtual:number = 0;
+
+  ngOnInit(): void {
+    this.filmexExibidos = this.filmes.slice(0,this.totalFilmes)
+  }
+
+  public nextFilmes(): void {
+    this.totalFilmes ++
+    this.paginaAtual ++
+    this.atualizarFilmes()
+  }
+
+  public returnFilmes(): void {
+    this.totalFilmes --;
+    this.paginaAtual --;
+    this.atualizarFilmes()
+  }
+
+  private atualizarFilmes(): void {
+    this.filmexExibidos = this.filmes.slice(this.paginaAtual, this.totalFilmes)
+  }
+
 
 }
