3e2c11547dd8296f6e6eafd89df07e1bf9f6b796
O-orion <lucas.lvtorres@gmail.com>
2024-03-28 13:27:53 -0300

    input pesquisa menu

--- over-app/src/components/navBar.tsx
+++ over-app/src/components/navBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Menu } from "native-base";
+import { Box, Text, Menu, Input, Icon } from "native-base";
 import { TEMAS } from "../styles/temas";
 import { Pressable } from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -10,9 +10,14 @@ export default function NavBar ( ) {
         <Text fontStyle='italic' color={TEMAS.colors.red}>Flix</Text>
       </Text>
 
-      <Menu color={TEMAS.colors.silver}   trigger={triggerProps => {
+      <Box display='flex' flexDir='row' justifyContent='center' alignItems='center' alignContent='center'>
+        <Input pl={2} borderColor={TEMAS.colors.gold} color={TEMAS.colors.silver} w='78%' placeholder="Pesquise..." h={8}
+        InputRightElement={<Ionicons mr="2" size={18} color={TEMAS.colors.gold} name="search-outline" ></Ionicons>}
+        ></Input>
+
+        <Menu fontSize={2} color={TEMAS.colors.silver}   trigger={triggerProps => {
         return <Pressable accessibilityLabel="Mais opções" {...triggerProps} >
-          <Ionicons name="menu-outline" size={30} color={TEMAS.colors.white}>
+          <Ionicons  name="menu-outline" size={30} color={TEMAS.colors.white}>
 
           </Ionicons>
         </Pressable>
@@ -23,7 +28,8 @@ export default function NavBar ( ) {
           <Menu.Item>Perfil</Menu.Item>
           <Menu.Item>Configurações</Menu.Item>
         </Menu.Group>
-      </Menu>
+        </Menu>
+      </Box>
 
     </Box>
   )
